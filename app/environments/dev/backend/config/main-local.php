290e66076db8fc4e1d7e72a9f704ee85dd232a25
HV220 <diagenin@gmail.com>
2023-02-02 00:15:55 +0300

    files redacted

--- app/environments/dev/backend/config/main-local.php
+++ app/environments/dev/backend/config/main-local.php
@@ -14,27 +14,4 @@ $config = [
     ],
 ];
 
-if (YII_ENV_DEV) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => DebugModule::class,
-        'allowedIPs' => ['*'],
-    ];
-
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => Module::class,
-        'allowedIPs' => ['*'],
-        'generators' => [
-            'crud' => [
-                'class' => 'yii\gii\generators\crud\Generator',
-                'templates' => [
-                    'yii2-adminlte3' => '@vendor/hail812/yii2-adminlte3/src/gii/generators/crud/default',
-                ]
-            ]
-        ],
-    ];
-}
-
 return $config;
