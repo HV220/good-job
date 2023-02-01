f5dfa6aff5633efdb06703d9ad7091b954be19f1
HV220 <diagenin@gmail.com>
2023-02-02 00:05:57 +0300

    files redacted

--- app/environments/dev/backend/web/index.php
+++ app/environments/dev/backend/web/index.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 use yii\helpers\ArrayHelper;
 use yii\web\Application;
 
-defined('YII_DEBUG') or define('YII_DEBUG', false);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+//defined('YII_DEBUG') or define('YII_DEBUG', false);
+//defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
